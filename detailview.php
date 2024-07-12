9d05d53c7c4a4cbd406c413ebd0c8b8551a311f8
ambo18 <jherietandugon@gmail.com>
2024-07-12 13:01:54 +0800

    Add PAG-IBIG No.

--- detailview.php
+++ detailview.php
@@ -180,6 +180,12 @@
 				</tbody>
 				<tbody>
 					<tr>
+						<td style="text-align: right;"><b>PAG-IBIG NO.</b> ::</td>
+						<td><?php if($row) { echo $row['PAGIBIGNO']; } else{ echo "Null"; }?></td>
+					</tr>
+				</tbody>
+				<tbody>
+					<tr>
 						<td style="text-align: right;"><b>Department</b> ::</td>
 						<td><?php if($row) { echo $row['Department']; } else{ echo "Null"; }?></td>
 					</tr>
